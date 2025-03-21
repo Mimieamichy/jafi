b685eff3925bdddcefb36d561e79b273874f6470
Mimieamichy <amichy07@gmail.com>
2025-03-21 17:22:56 +0100

    fiex footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./NavBar";
 import Homepage from "./pages/Homepage";
-import SignupPage from "./SignupPage";
 import HiringSignup from "./HiringSignup";
 import BusinessSignup from "./BusinessSignup";
 import PaginatedReviews from "./ReviewsPage";
@@ -20,30 +19,35 @@ import BusinessProfileCard from "./BusinessProfileCard";
 export default function App() {
   return (
     <Router>
-      <Navbar />
-      <ReviewsProvider>
-        <div className="pt-9 mt-10">
-          <Routes>
-            <Route path="/" element={<Homepage />} />
-            <Route path="/hiring-signup" element={<HiringSignup />} />
-            <Route path="/business-signup" element={<BusinessSignup />} />
-            <Route path="/review-page" element={<PaginatedReviews />} />
-            <Route path="/signin" element={<SignIn />} />
-            <Route path="/signup" element={<SignupModal />} />
-            <Route path="/pricing" element={<Pricing />} />
-            <Route path="/hiring-payment" element={<HiringPayment />} />
-            <Route path="/hiring-dashboard" element={<HiringDashboard />} />
-            <Route path="/hire-profile" element={<HireProfileCard />} />
-            <Route path="/bus-dashboard" element={<BusinessDashboard />} />
-            <Route path="/bus-profile" element={<BusinessProfileCard />} />
-            <Route path="/business/:businessName" element={<BusinessPage />} />
-          </Routes>
-          <div id="contact" className="bottom-0 mt-26 sm:mt-0">
-            {" "}
-            <Footer />
+      <div className="flex flex-col min-h-screen">
+        <Navbar />
+        <ReviewsProvider>
+          {/* Main content should grow to push the footer down */}
+          <div className="flex-grow pt-9 mt-10">
+            <Routes>
+              <Route path="/" element={<Homepage />} />
+              <Route path="/hiring-signup" element={<HiringSignup />} />
+              <Route path="/business-signup" element={<BusinessSignup />} />
+              <Route path="/review-page" element={<PaginatedReviews />} />
+              <Route path="/signin" element={<SignIn />} />
+              <Route path="/signup" element={<SignupModal />} />
+              <Route path="/pricing" element={<Pricing />} />
+              <Route path="/hiring-payment" element={<HiringPayment />} />
+              <Route path="/hiring-dashboard" element={<HiringDashboard />} />
+              <Route path="/hire-profile" element={<HireProfileCard />} />
+              <Route path="/bus-dashboard" element={<BusinessDashboard />} />
+              <Route path="/bus-profile" element={<BusinessProfileCard />} />
+              <Route
+                path="/business/:businessName"
+                element={<BusinessPage />}
+              />
+            </Routes>
           </div>
-        </div>
-      </ReviewsProvider>
+        </ReviewsProvider>
+
+        {/* Footer always at bottom */}
+        <Footer />
+      </div>
     </Router>
   );
 }
