6fce02fd3f49b632fd298176614e09ee4c78ae85
Amazingmercy <amaefulemercy499@gmail.com>
2025-04-02 19:34:11 +0200

    worked on some admin functionalities

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,16 @@
 require("dotenv").config();
 const app = require("./app");
 
-const PORT = process.env.PORT || 4900;
+const PORT = process.env.PORT || 5173;
+
+
+// Serve static frontend files
+app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+// Handle all unknown routes by serving index.html
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/build", "index.html"));
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
