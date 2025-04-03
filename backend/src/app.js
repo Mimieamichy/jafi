2a68a5e28c0c038f2eaef3e5ba70e9104dc2b3f2
Mimieamichy <amichy07@gmail.com>
2025-04-03 11:56:58 +0100

    modify server to be serve client

--- backend/src/app.js
+++ backend/src/app.js
@@ -62,11 +62,11 @@ app.get("/", (req, res) => {
 });
 
 // Serve static frontend files
-app.use(express.static(path.join(__dirname, "../frontend/build")));
+app.use(express.static(path.join(__dirname, 'frontend', 'dist')));
 
 // Handle all unknown routes by serving index.html
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend/build", "index.html"));
+    res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'));
 });
 
 // Error Handling Middleware
