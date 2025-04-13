d36d5b99043f9a744945cdd26cf550224a3676d7
Amazingmercy <amaefulemercy499@gmail.com>
2025-04-13 20:48:38 +0200

    Reviewed and corrected the review module

--- backend/src/domains/review/review.model.js
+++ backend/src/domains/review/review.model.js
@@ -58,7 +58,7 @@ const Review = sequelize.define(
       allowNull: false,
       defaultValue: true,
     },
-    reply: {
+    replyId: {
       type: DataTypes.INTEGER,
       allowNull: true,
       references: {
