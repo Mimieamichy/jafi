f05409625bcf1b8cba6fdf94b41cf363e33fea93
Amazingmercy <amaefulemercy499@gmail.com>
2025-04-30 16:21:21 +0200

    Modified the admin module

--- backend/src/utils/exports.js
+++ backend/src/utils/exports.js
@@ -5,6 +5,7 @@ exports.exportTableData = async (model, filename, res) => {
     const data = await model.findAll();
     const jsonData = data.map(item => item.toJSON());
 
+    
     if (jsonData.length === 0) {
       return res.status(404).json({ message: `No ${filename} data found` });
     }
