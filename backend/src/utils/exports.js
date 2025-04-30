6fb3581b60f9179907ce03763db6f821d1bcb8c4
Amazingmercy <amaefulemercy499@gmail.com>
2025-04-30 16:41:50 +0200

    Modified the admin module

--- backend/src/utils/exports.js
+++ backend/src/utils/exports.js
@@ -5,15 +5,13 @@ exports.exportTableData = async (model, filename, res) => {
     const data = await model.findAll();
     const jsonData = data.map(item => item.toJSON());
 
-    
+
     if (jsonData.length === 0) {
       return res.status(404).json({ message: `No ${filename} data found` });
     }
 
     const csv = await writeToString(jsonData);
 
-    res.setHeader('Content-Type', 'text/csv');
-    res.setHeader('Content-Disposition', `attachment; filename=${filename}.csv`);
     res.status(200).send(csv);
   } catch (error) {
     console.error(`Error exporting ${filename}:`, error);
